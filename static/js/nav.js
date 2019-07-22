4ab5682f007834dc9cfca13fa51d2a2c86becdb1
jappoker <walkerinvrains@gmail.com>
2019-07-22 00:16:39 -0400

    publication search

--- static/js/nav.js
+++ static/js/nav.js
@@ -4,6 +4,10 @@ if (document.getElementById("sidebar")){
 var sidebar = document.getElementById("sidebar");
 };
 
+if (document.getElementById("search-pub")) {
+    var searchpub = document.getElementById("search-pub");
+};
+
 function scrollFunction() {
     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
         
@@ -84,5 +88,74 @@ function peopleless() {
     pplmore.classList.add("show");
     pplless.classList.remove("show");
     pplless.classList.add("hidden");
+
+};
+
+function publication_filter() {
+
+    var input, filter, pub, ul, ttl, name,year,journal, i,j, txtValue, nmValue,yValue,jValue,showornot;
+    input = document.getElementById('search-pub');
+    filter = input.value.toLowerCase() ;
+    pub = document.getElementById("publication");
+    ul = pub.getElementsByTagName('ul');
+
+    for (i = 0; i < ul.length; i++) {
+        ttl = ul[i].getElementsByTagName("li")[0] ;
+        name =  ul[i].getElementsByTagName("li")[1];
+        year = ul[i].getElementsByTagName("li")[2];
+        journal = ul[i].getElementsByTagName("li")[3];
+        txtValue = ttl.textContent || ttl.innerText;
+        nmValue = name.textContent || name.innerText;
+        yValue = year.textContent || year.innerText;
+        jValue = journal.textContent || journal.innerText;
+        if (txtValue.toLowerCase().indexOf(filter) > -1)  {
+            ul[i].style.display = "";
+        } else {
+            if (nmValue.toLowerCase().indexOf(filter) > -1) {
+                ul[i].style.display = "";
+            }
+            else {
+                if (yValue.toLowerCase().indexOf(filter) > -1){
+                    ul[i].style.display = "";
+                }
+                else{
+                    if (jValue.toLowerCase().indexOf(filter) > -1){
+                        ul[i].style.display = "";
+                    }
+                    else{
+                        ul[i].style.display = "none";
+                    }
+                }
+            
+            }
+        }
+        
+    };
+
+    var pubcase = document.getElementsByClassName("publication-year");
+
+    
+    for (i=0 ; i<pubcase.length; i++){
+        
+        ul = pubcase[i].getElementsByTagName('ul');
+        showornot = false;
+            for (j = 0; j < ul.length; j++) {
+                
+                if (ul[j].style.display != "none"){
+                    showornot = true;
+                }
+
+            };
+            if (showornot ){
+                pubcase[i].style.display = "";
+            }
+            else{
+                pubcase[i].style.display = "none";
+            };
+    
+
+
+    };
+
 
 };
