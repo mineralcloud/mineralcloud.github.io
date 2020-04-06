259e5d5c9b2ed0a6bd78f2e1409ef910eba2ffcd
jappoker <walkerinvrains@gmail.com>
2020-04-06 16:10:54 -0400

    js updated

--- static/js/vlab_iframe.js
+++ static/js/vlab_iframe.js
@@ -22,7 +22,7 @@ function fit_iframe(iframe_id){
     var iFrameID = document.getElementById(iframe_id);
     if (iFrameID) {
         iFrameID.height = "";
-        var newHeight = iFrameID.contentWindow.document.body.scrollHeight;
+        var newHeight = iFrameID.contentWindow.document.body.height;
         newHeight = parseInt(newHeight) + 60;
         iFrameID.height = newHeight + "px";
     }
