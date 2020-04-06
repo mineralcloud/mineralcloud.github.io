2a7ba3b8c438535dd6c5ce73de1c72bbba2b6303
jappoker <walkerinvrains@gmail.com>
2020-04-06 16:30:45 -0400

    js updated

--- static/js/vlab_iframe.js
+++ static/js/vlab_iframe.js
@@ -3,29 +3,20 @@ function load_iframe(iframe_id){
     if (iFrameID) {
         if (iframe_id == "vlab_people"){
             var dir = "https://mineralcloud.github.io/people/vlab_people.html";
+            var height = 5050;
         }
         if (iframe_id == "vlab_publication") {
             var dir = "https://mineralcloud.github.io/publications/vlab_publications.html";
+            var height = 19520;
         }
         if (iframe_id == "vlab_events") {
             var dir = "https://mineralcloud.github.io/events/vlab_events.html";
+            var height = 6200;
         }
 
         iFrameID.src = dir;
+        iFrameID.height = height;
 
         
-        fit_iframe(iframe_id);
     }
-}
-
-function fit_iframe(iframe_id){
-    var iFrameID = document.getElementById(iframe_id);
-    if (iFrameID) {
-        iFrameID.height = "";
-        var newHeight = iFrameID.contentWindow.document.body.scrollHeight;
-        
-        newHeight = newHeight + 60;
-        iFrameID.height = newHeight + "px";
-    }
-
 }
