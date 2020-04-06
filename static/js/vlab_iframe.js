d97b8ceb3ca838692de80a29e921b26772976686
jappoker <walkerinvrains@gmail.com>
2020-04-06 15:50:58 -0400

    js updated

--- static/js/vlab_iframe.js
+++ static/js/vlab_iframe.js
@@ -2,7 +2,7 @@ function load_iframe(iframe_id){
     var iFrameID = document.getElementById(iframe_id);
     if (iFrameID) {
         if (iframe_id == "vlab_people"){
-            var dir = "https://mineralcloud.github.io/people/vlab_people";
+            var dir = "https://mineralcloud.github.io/people/vlab_people.html";
         }
         if (iframe_id == "vlab_publication") {
             var dir = "https://mineralcloud.github.io/publications/vlab_publications.html";
@@ -19,9 +19,9 @@ function load_iframe(iframe_id){
 function fit_iframe(iframe_id){
     var iFrameID = document.getElementById(iframe_id);
     if (iFrameID) {
-        // here you can make the height, I delete it first, then I make it again
         iFrameID.height = "";
-        var newHeight = iFrameID.contentWindow.document.body.scrollHeight + 50;
+        var newHeight = iFrameID.contentWindow.document.body.scrollHeight;
+        newHeight = parseInt(newHeight) + 60;
         iFrameID.height = newHeight + "px";
     }
 
